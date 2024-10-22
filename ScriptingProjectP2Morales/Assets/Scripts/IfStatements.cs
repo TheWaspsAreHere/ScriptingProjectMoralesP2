8f37cb4fc3c856da0ef1587222039636c989f39a
TheWaspsAreHere <719045@student.dusd.net>
2024-10-22 09:57:37 -0700

    New scene
    Added a new practice scripting project

---
+++ ScriptingProjectP2Morales/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+
+    void TemperatureTest()
+    {
+        // If it isn't, but the coffee temperature is less than the coldest drinking temperture...
+         else if (coffeeTemperature < coldLimitTemperature)
+        {
+            // ... do this.
+            print("Coffee is too hot.");
+        }
+        // If it isn't, but the coffee temperature is less than the coldest drinking temperature...
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
+            // ... do this.
+            print("Coffee is too cold.");
+        }
+        // If it is neither to those then...
+        else
+        {
+            // ... do this.
+            print("Coffee is just right.");
+        }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
