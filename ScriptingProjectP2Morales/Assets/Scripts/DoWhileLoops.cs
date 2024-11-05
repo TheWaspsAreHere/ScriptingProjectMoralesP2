3b6dca9f5d1461edaa4a20ec5a60022bec2d5742
TheWaspsAreHere <719045@student.dusd.net>
2024-11-05 09:59:21 -0800

    Made damagables
    making enemies  for ruby, sadly didn't finish

--- ScriptingProjectP2Morales/Assets/Scripts/DoWhileLoops.cs
+++ ScriptingProjectP2Morales/Assets/Scripts/DoWhileLoops.cs
@@ -7,8 +7,15 @@ public class DoWhileLoops : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello World");
+
+        }while(shouldContinue == true);
     }
+
 
     // Update is called once per frame
     void Update()
