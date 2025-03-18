e08c53b55b3ad640550de3a7e11b5bdc6eccf4be
unicornglitterpoop <719045@student.dusd.net>
2025-03-18 09:19:05 -0700

    Get component

---
+++ ScriptingProjectP2Morales/Assets/Scripts/GetAxis.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetAxis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
